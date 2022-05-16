0828c24e2bcdd5c8a6d58fc9c693f953221c7c53
Mike-Scarlet <mike_scarlet@126.com>
2022-05-16 09:47:22 +0800

    stash

--- decorators.py
+++ decorators.py
@@ -1,6 +1,6 @@
 import time
 
-def CatchExceptionTillSuccessDecorator(f):
+def CatchExceptionTillSuccessDecorator(f, verbose=True):
   def decorated(*args, **kwargs):
     try_count = 0
     try_count_max = 20
@@ -8,9 +8,10 @@ def CatchExceptionTillSuccessDecorator(f):
       try:
         return f(*args, **kwargs)
       except Exception as e:
-        print("exception has raised in {}".format(f.__name__))
-        print("type:", type(e))
-        print(e)
+        if verbose:
+          print("exception has raised in {}".format(f.__name__))
+          print("type:", type(e))
+          print(e)
         try_count += 1
         time.sleep((try_count / try_count_max) * 30)
         continue
